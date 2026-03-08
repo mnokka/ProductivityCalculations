dc74ce10fe6e04d0ddc56a12fa9748c107f6e4ee
Mika Nokka <mika.nokka1@gmail.com>
2026-03-08 17:47:13 +0200

    Combine two sheets

--- browser.py
+++ browser.py
@@ -1,36 +1,27 @@
 import pandas as pd
 
 money = pd.read_excel("money.xlsx", sheet_name="Sheet 1",header=9) # rivit 0-9 pois , rivi 10 alkaen
-money = money.loc[:, ~money.columns.str.contains('Unnamed')] # remove unnanamed after the year 
+money = money.loc[:, ~money.columns.str.contains('Unnamed')] # remove "unnanamed" after the year 
 
 workers = pd.read_excel("workers.xlsx", sheet_name="Sheet 1", header=9)
 workers = workers.loc[:, ~workers.columns.str.contains('Unnamed')] 
 
 
-def delimeter():
-    print("-----------------------------------------------------------------------------")
+def delimiter():
+    print("-" * 40)
 
 
-
-
+def print_data(df, name):
+    print(name.upper())
+    for year in ["2018", "2019"]:
+        print(df[["TIME", year]])
+        delimiter()
 
 
 
 wanted_countries = ["Belgium", "Bulgaria", "Czechia", "Denmark", "Germany","Finland"]
 subset_money = money[money["TIME"].isin(wanted_countries)]
-
-print("MONEYS")
-print(subset_money[["TIME", "2018"]])
-delimeter()
-print(subset_money[["TIME", "2019"]])
-delimeter()
-
-
-
 subset_workers = workers[workers["TIME"].isin(wanted_countries)]
 
-print ("WORKERS")
-print(subset_workers[["TIME", "2018"]])
-delimeter()
-print(subset_workers[["TIME", "2019"]])
-delimeter()
+print_data(subset_money, "MONEYS")
+print_data(subset_workers, "WORKERS")
